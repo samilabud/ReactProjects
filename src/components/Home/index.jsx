f51b2b2c8a33b879f8efc98342ca77f9d1fd1fbf
sabud <sabud@pinterest.com>
2023-02-14 12:08:55 -0400

    Fixed bug and added home scroll to show modules

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,65 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import "animate.css";
 import "./home.css";
+import { NavLink } from "react-router-dom";
 
-const Home = () => <div>Home</div>;
+const Home = () => {
+  const ref = useRef(null);
+
+  const handleScroll = () => {
+    const boxes = ref.current.querySelectorAll(".box");
+    const triggerBottom = (window.innerHeight / 6) * 6;
+    boxes.forEach((box) => {
+      const boxTop = box.getBoundingClientRect().top;
+
+      if (boxTop < triggerBottom) {
+        box.classList.add("show");
+      } else {
+        box.classList.remove("show");
+      }
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+  }, []);
+
+  return (
+    <div id="homeContainer" ref={ref}>
+      <div className="homeTitle">Home - Scroll to see all proyects</div>
+      <div className="box">
+        <NavLink to="/expanding-cards">Expanding Cards</NavLink>
+      </div>
+      <div className="box">
+        <NavLink to="/blurry-loading">Blurry Loading</NavLink>
+      </div>
+      <div className="box">
+        <NavLink to="/expanding-cards">Expanding Cards</NavLink>
+      </div>
+      <div className="box">
+        <NavLink to="/blurry-loading">Blurry Loading</NavLink>
+      </div>
+      <div className="box">
+        <NavLink to="/expanding-cards">Expanding Cards</NavLink>
+      </div>
+      <div className="box">
+        <NavLink to="/blurry-loading">Blurry Loading</NavLink>
+      </div>
+      <div className="box">
+        <NavLink to="/expanding-cards">Expanding Cards</NavLink>
+      </div>
+      <div className="box">
+        <NavLink to="/blurry-loading">Blurry Loading</NavLink>
+      </div>
+      <div className="box">
+        <NavLink to="/expanding-cards">Expanding Cards</NavLink>
+      </div>
+      <div className="box">
+        <NavLink to="/blurry-loading">Blurry Loading</NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
